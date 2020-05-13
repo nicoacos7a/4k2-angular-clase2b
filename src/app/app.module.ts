8874cef61bed0d9632dcc2f3653594ccc97a5f2e
nicoacos7a <nicoacos7a@gmail.com>
2020-05-13 19:59:17 -0300

    Creacion principal - Elim appComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { MenuComponent } from './menu/menu.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MenuComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ MenuComponent, PrincipalComponent ],
+  bootstrap:    [ PrincipalComponent ]
 })
 export class AppModule { }
---
+++ src/app/principal/principal.component.ts
@@ -0,0 +1,19 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent implements OnInit {
+
+  mensaje: string;
+  materia: 'PAV II';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
