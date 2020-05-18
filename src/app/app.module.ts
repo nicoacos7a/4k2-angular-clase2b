cef7125b335491d55678027b21a399b6973c3cca
nicoacos7a <nicoacos7a@gmail.com>
2020-05-18 01:41:35 -0300

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { MenuComponent } from './menu/menu.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { ArticulosListaComponent } from './articulos-lista/articulos-lista.component';
+import { RouterModule } from '@angular/router';
+import { ArticulosCargaComponent } from './articulos-carga/articulos-carga.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ MenuComponent, PrincipalComponent, ArticulosListaComponent ],
+  imports: [ BrowserModule, FormsModule, RouterModule.forRoot([
+    {path: 'listaarticulos', component: ArticulosListaComponent},
+    {path: 'cargaarticulos', component: ArticulosCargaComponent},
+    {path: 'principal', component: PrincipalComponent},
+    {path: '', component: PrincipalComponent}
+    ])],
+  declarations: [ MenuComponent, PrincipalComponent, ArticulosListaComponent, ArticulosCargaComponent ],
   bootstrap:    [ PrincipalComponent ]
 })
 export class AppModule { }
---
+++ src/app/articulos-carga/articulos-carga.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-articulos-carga',
+  templateUrl: './articulos-carga.component.html',
+  styleUrls: ['./articulos-carga.component.css']
+})
+export class ArticulosCargaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
